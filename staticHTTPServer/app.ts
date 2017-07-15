0632e507eb87b290a9ff1d491b56d7bea3368698
nate willard <nate.a.willard@gmail.com>
2017-07-15 15:42:31 -0700

    working basic 200 http get

--- staticHTTPServer/app.ts
+++ staticHTTPServer/app.ts
@@ -1,14 +1,28 @@
 'use strict';
 
+// import * as async from 'async';
 import * as net from 'net';
+import { response } from './responses';
 
 const HOST = '127.0.0.1';
 const PORT = 8080;
 
 console.log('starting socket listener');
+
 net.createServer(socket => {
-  console.log('socket.remoteAddress', socket.remoteAddress);
-  console.log('socket.remotePort', socket.remotePort);
+  socket.on('data', (data: Buffer) => {
+    socket.write(Buffer.from(response), (err: string, res: boolean) => {
+      if (err) {
+        console.log(`error writing tcp response ${err}`);
+        process.exit(1);
+      }
+      socket.destroy();
+    });
+  });
+
+  socket.on('close', data => {
+    console.log('close data', data);
+  });
 }).listen(PORT, HOST);
 
 
---
+++ staticHTTPServer/responses.ts
@@ -0,0 +1,8 @@
+const response: string = `HTTP/1.1 200 OK
+  Content-type: text/html
+
+  <H1>Success!</H1>`;
+
+export {
+  response
+};
