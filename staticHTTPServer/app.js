408798b884ca66f668f4d29a2ecc9e08e5c1b1c8
nate willard <nate.a.willard@gmail.com>
2017-07-15 15:06:17 -0700

    wip

--- staticHTTPServer/app.js
+++ staticHTTPServer/app.js
@@ -1,1 +1,10 @@
 'use strict';
+Object.defineProperty(exports, "__esModule", { value: true });
+var net = require("net");
+var HOST = '127.0.0.1';
+var PORT = 8080;
+console.log('starting socket listener');
+net.createServer(function (socket) {
+    console.log('socket.remoteAddress', socket.remoteAddress);
+    console.log('socket.remotePort', socket.remotePort);
+}).listen(PORT, HOST);
--- staticHTTPServer/app.ts
+++ staticHTTPServer/app.ts
@@ -1,5 +1,15 @@
 'use strict';
 
-// DOING SOME BORING THINGS AROUND RECONFIGURING EDITORS / TYPESCRIPT
-// SHRUG 
+import * as net from 'net';
+
+const HOST = '127.0.0.1';
+const PORT = 8080;
+
+console.log('starting socket listener');
+net.createServer(socket => {
+  console.log('socket.remoteAddress', socket.remoteAddress);
+  console.log('socket.remotePort', socket.remotePort);
+}).listen(PORT, HOST);
+
+
 
